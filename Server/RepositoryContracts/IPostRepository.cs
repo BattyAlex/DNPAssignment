d9f7f0232c4877b8dd5fb86b0b8889188a3db15c
SophiaJustin <150365571+SophiaJustin@users.noreply.github.com>
2024-09-04 12:59:56 +0200

    Update IPostRepository.cs
    Updated Post

--- Server/RepositoryContracts/IPostRepository.cs
+++ Server/RepositoryContracts/IPostRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace RepositoryContracts;
+﻿using Entities;
+
+namespace RepositoryContracts;
 
 public interface IPostRepository
 {
-    
+   Task<Post> AddPost(Post post);
+   Task<Post> UpdatePost(Post post);
+   Task<Post> DeletePost(int  ID);
+   Task<Post> GetSinglePost(int ID);
+   IQueryable<Post> GetMultiplePosts();
+   
 }
