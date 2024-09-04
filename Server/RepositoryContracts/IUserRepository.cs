2bde49b4260317277867eaf09c7fcbb17f8c73ff
CodeAFox <illeskrisztinacsilla@gmail.com>
2024-09-04 12:57:29 +0200

    User Repository Interface added

--- Server/RepositoryContracts/IUserRepository.cs
+++ Server/RepositoryContracts/IUserRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace RepositoryContracts;
+﻿using Entities;
+
+namespace RepositoryContracts;
 
 public interface IUserRepository
 {
-    
+    Task<User> AddUserAsync(User user);
+    Task UpdateUserAsync(User user);
+    Task DeleteUserAsync(int id);
+    Task<User> GetSingleUserAsync(int id);
+    IQueryable<User> GetManyUsersAsync();
 }
