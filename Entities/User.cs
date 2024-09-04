fab410984fd3f0d39eea5eafe9951d921c177118
SophiaJustin <150365571+SophiaJustin@users.noreply.github.com>
2024-09-04 12:30:58 +0200

    Updated User

--- Entities/User.cs
+++ Entities/User.cs
@@ -5,4 +5,11 @@ public class User
    public int ID{get; set;} 
    public string Name{get; set;}
    public string Password{get; set;}
+
+   public User(int id, string name, string password)
+   {
+      this.ID = id;
+      this.Name = name;
+      this.Password = password;
+   }
 }
