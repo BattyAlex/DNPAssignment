03e7c8acd0496929a4d3dd844b1498f724eb9f20
BattyAlex <Alexandriaahansen@gmail.com>
2024-09-04 12:40:13 +0200

    Fucking Comment

--- Entities/Comment.cs
+++ Entities/Comment.cs
@@ -2,16 +2,16 @@
 
 public class Comment
 {
-    public int Id {get; set;}
-	public string CommentBody{get; set;} 
-	public int UserId {get; set;}
-	public int PostId {get; set;}
+        public int Id {get; set;}
+        public string CommentBody{get; set;} 
+        public int UserId {get; set;}
+        public int PostId {get; set;}
 
-	public Comment(int id, string body, int userId, int postId)
-	{
-		Id = id;
-		CommentBody = body;
-		UserId = userId;
-		PostId = postId;
-	}
+        public Comment(int id, string body, int userId, int postId)
+        {
+            Id = id;
+            CommentBody = body;
+            UserId = userId;
+            PostId = postId;
+        }
 }
