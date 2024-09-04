abfadcc1003d08512f018e079c5fe6326ea04ded
AlexaKelemen <kelemenalexa85@gmail.com>
2024-09-04 12:31:31 +0200

    Update Post.cs

--- Entities/Post.cs
+++ Entities/Post.cs
@@ -6,4 +6,11 @@ public class Post
     public string Title { get; set; }
     public string Content { get; set; }
     public string UserID { get; set; }
+    public Post(int id, string title, string content, string userId)
+    {
+        ID = id;
+        Title = title;
+        Content = content;
+        UserID = userId;
+    }
 }
