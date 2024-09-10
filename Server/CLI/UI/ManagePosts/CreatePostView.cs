6e22cd751eb44b2ec41e45c1025078542a2d334d
SalomeeaTricolici <149162841+SalomeeaTricolici@users.noreply.github.com>
2024-09-10 11:19:29 +0200

    Update CreatePostView.cs

--- Server/CLI/UI/ManagePosts/CreatePostView.cs
+++ Server/CLI/UI/ManagePosts/CreatePostView.cs
@@ -1,6 +1,14 @@
-﻿namespace CLI.UI.ManagePosts;
+﻿using RepositoryContracts;
+
+namespace CLI.UI.ManagePosts;
 
 public class CreatePostView
 {
-    
+    private readonly IPostRepository postRepository;
+
+    public CreatePostView(IPostRepository postRepository)
+    {
+        this.postRepository = postRepository;
+    }
+
 }
