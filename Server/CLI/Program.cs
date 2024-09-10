f3a4871ad0fb71f60fc9610a47fba2cbf2bcfaaa
BattyAlex <Alexandriaahansen@gmail.com>
2024-09-10 14:53:53 +0200

    Trial edit

--- Server/CLI/Program.cs
+++ Server/CLI/Program.cs
@@ -1,6 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Welcome to Reddat \nPlease enter your username");
-string? username = Console.ReadLine();
-
 
---
+++ Server/CLI/UI/ManageUsers/LoginUserView.cs
@@ -0,0 +1,19 @@
+﻿namespace CLI.UI.ManageUsers;
+
+public class LoginUserView
+{
+    public void Start()
+    {
+        Console.WriteLine("1 - Create user \n2 - Login");
+        int? input = Console.Read();
+        if(input is null)
+            Console.WriteLine("Bad");
+        if (input == 1)
+        {
+              //switch view
+        }
+        if (input == 2)
+        {
+        }
+    }
+}
