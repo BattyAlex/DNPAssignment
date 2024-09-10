1d66144731db0c6361ff6b120f426c06f5ef3958
SophiaJustin <150365571+SophiaJustin@users.noreply.github.com>
2024-09-10 11:29:01 +0200

    Creating User view

--- Entities/User.cs
+++ Entities/User.cs
@@ -12,4 +12,10 @@ public class User
       Name = name;
       Password = password;
    }
+
+   public User(string password, string name)
+   {
+      Password = password;
+      Name = name;
+   }
 }
--- Server/CLI/UI/ManageUsers/CreateUserView.cs
+++ Server/CLI/UI/ManageUsers/CreateUserView.cs
@@ -1,6 +1,29 @@
-﻿namespace CLI.UI.ManageUsers;
+﻿using Entities;
+using InMemoryRepositories;
+using RepositoryContracts;
+
+namespace CLI.UI.ManageUsers;
 
 public class CreateUserView
 {
+    private readonly IUserRepository _userRepository;
+
+    public CreateUserView(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("Create New User");
+        Console.WriteLine("Enter Username:");
+        string username = Console.ReadLine();
+        Console.WriteLine("Enter Password:");
+        string password = Console.ReadLine();
+        
+        User newUser = new User(password, username);
+        _userRepository.AddUserAsync(newUser);
+    }
+
     
 }
