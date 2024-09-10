0a6322c36c98955ac681b3b39a3c810c3f467f75
SophiaJustin <150365571+SophiaJustin@users.noreply.github.com>
2024-09-10 11:43:22 +0200

    Get post by single id

--- Server/CLI/UI/ManagePosts/SinglePostView.cs
+++ Server/CLI/UI/ManagePosts/SinglePostView.cs
@@ -1,6 +1,28 @@
-﻿namespace CLI.UI.ManagePosts;
+﻿using Entities;
+using RepositoryContracts;
+
+namespace CLI.UI.ManagePosts;
 
 public class SinglePostView
 {
-    
+    private readonly IPostRepository _postRepository;
+
+    public SinglePostView(IPostRepository postRepository)
+    {
+        _postRepository = postRepository;
+    }
+
+    public async Task ShowPostById(int postId)
+    {
+        Post post = await _postRepository.GetSinglePostAsync(postId);
+        if (post != null)
+        {
+            Console.WriteLine("Title: {post.Title}", post.Title);
+            Console.WriteLine("Content: {post.Content}", post.Content);
+        }
+        else
+        {
+            Console.WriteLine("Post not found");
+        }
+    }
 }
