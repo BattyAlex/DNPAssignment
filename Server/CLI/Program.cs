4ebfa9cc300c6e62eff8571d8cb3a2b5b41fa7ea
BattyAlex <Alexandriaahansen@gmail.com>
2024-09-10 11:12:21 +0200

    Update Program.cs

--- Server/CLI/Program.cs
+++ Server/CLI/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Welcome to Redat \nPlease enter your username");
+Console.WriteLine("Welcome to Reddat \nPlease enter your username");
 string? username = Console.ReadLine();
 
 
