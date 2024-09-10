590f9507732ec247a73d48a639e9e0d6f118a028
SalomeeaTricolici <149162841+SalomeeaTricolici@users.noreply.github.com>
2024-09-10 11:29:47 +0200

    Update CreatePostView.cs
    not sure what im doing

--- Server/CLI/UI/ManagePosts/CreatePostView.cs
+++ Server/CLI/UI/ManagePosts/CreatePostView.cs
@@ -11,4 +11,18 @@ public class CreatePostView
         this.postRepository = postRepository;
     }
 
+    public class ExecuteCommand()
+    {
+         public CreatePostView createPostView { get; set; }
+    }
+
+    public void CreatePost()
+    {
+        Console.WriteLine("Enter the title of the post:");
+        string? title = Console.ReadLine();
+
+        Console.WriteLine("Enter the content of the post:");
+        string? content = Console.ReadLine();
+    }
+
 }
