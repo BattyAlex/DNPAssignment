c8192f7bbf1ed5329cafcccbf3cc03e4989ca778
CodeAFox <illeskrisztinacsilla@gmail.com>
2024-09-11 13:13:25 +0200

    UserLoggedIn.cs added

---
+++ Server/CLI/UI/UserLoggedIn.cs
@@ -0,0 +1,13 @@
+﻿using Entities;
+
+namespace CLI.UI;
+
+public class UserLoggedIn
+{
+    private User? userLoggedIn;
+
+    public void Login(User user)
+    {
+        userLoggedIn = user;
+    }
+}
