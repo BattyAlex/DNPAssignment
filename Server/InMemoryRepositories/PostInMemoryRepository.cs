6c880c4df5a91223d2a85cc96482372f57f00b97
CodeAFox <illeskrisztinacsilla@gmail.com>
2024-09-11 14:48:32 +0200

    Posts added as dummy data

--- Server/InMemoryRepositories/PostInMemoryRepository.cs
+++ Server/InMemoryRepositories/PostInMemoryRepository.cs
@@ -6,6 +6,18 @@ namespace InMemoryRepositories;
 public class PostInMemoryRepository : IPostRepository
 {
     public List<Post> Posts { get; set; } = new List<Post>();
+
+    public PostInMemoryRepository()
+    {
+        Post aboutDogs = new Post("About dogs",
+            "Did you know that dogs have four legs thus making them quadropeds? Some dogs have double coats which helps them keep insulated; warm during the winter and cold during summers. Shaving this double coat can be quite problematic as it'll never grow back in the way it should. Only shave double coated dogs if you have no other option!",
+            2);
+        Post aCollectionOfPuns = new Post("Punderful punciyclopedia",
+            "What's the difference between an arhaeologist and a grave raider?\nA diploma.\n\nWhat do you call a pile of cats?\nA meowntain.",
+            4);
+        AddPostAsync(aboutDogs);
+        AddPostAsync(aCollectionOfPuns);
+    }
     public Task<Post> AddPostAsync(Post post)
     {
         post.ID = Posts.Any() ? Posts.Max(p => p.ID) + 1 : 1;
