722e7768f09f6a8ec57b03a462ba4c4b55933c41
AlexaKelemen <kelemenalexa85@gmail.com>
2024-09-11 12:29:52 +0200

    Update ListPostView.cs

--- Server/CLI/UI/ManagePosts/ListPostView.cs
+++ Server/CLI/UI/ManagePosts/ListPostView.cs
@@ -1,4 +1,5 @@
-﻿using InMemoryRepositories;
+﻿using Entities;
+using InMemoryRepositories;
 using RepositoryContracts;
 
 namespace CLI.UI.ManagePosts;
@@ -6,7 +7,9 @@ namespace CLI.UI.ManagePosts;
 public class ListPostView
 {
     private ViewHandler viewHandler;
-    private readonly IPostRepository postRepository = new PostInMemoryRepository();
+
+    private readonly IPostRepository postRepository =
+        new PostInMemoryRepository();
 
     public ListPostView(ViewHandler viewHandler)
     {
@@ -16,6 +19,24 @@ public class ListPostView
     public void Show()
     {
         Console.WriteLine("Available Posts:");
+        var posts = postRepository.GetMultiplePosts().ToList();
+
+        if (!posts.Any())
+        {
+            Console.WriteLine("No posts available.");
+            return;
+        }
+        
+        foreach (var post in posts)
+        {
+            Console.WriteLine($"Post ID: {post.ID}");
+            Console.WriteLine($"Title: {post.Title}");
+            Console.WriteLine($"Body: {post.Content}\n");
+            Console.WriteLine(
+                "\n====================\n"); 
+        }
+
+        Console.WriteLine("Press any key to return to the menu.");
+        Console.ReadKey();
     }
-    
 }
