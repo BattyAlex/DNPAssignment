7f6e2be0ee486f5435b48c4008b6a50baa626631
SalomeeaTricolici <149162841+SalomeeaTricolici@users.noreply.github.com>
2024-09-17 10:09:01 +0200

    Update CommentFileRepository.cs

--- Server/FileRepositories/CommentFileRepository.cs
+++ Server/FileRepositories/CommentFileRepository.cs
@@ -1,4 +1,5 @@
-﻿using Entities;
+﻿using System.Text.Json;
+using Entities;
 using RepositoryContracts;
 
 namespace FileRepositories;
@@ -6,5 +7,24 @@ namespace FileRepositories;
 
 public class CommentFileRepository: ICommentRepository
 {
-    
+    private readonly string filePath = "comments.json";
+
+    public CommentFileRepository()
+    {
+        if (!File.Exists(filePath))
+        {
+            File.WriteAllText(filePath, "[]");
+        }
+    }
+
+    public async Task<List<Comment>> GetCommentsAsync(Comment comment)
+    {
+        string commentJson = await File.ReadAllTextAsync(filePath);
+        List<Comment> comments = JsonSerializer.Deserialize<List<Comment>>(commentsAsJson)!;
+        comment.Id = maxId + 1;
+        comments.Add(comment);
+        commentJson = JsonSerializer.Serialize(comments);
+        await File.WriteAllTextAsync(filePath, commentJson);
+        return comments;
+    }
 }
