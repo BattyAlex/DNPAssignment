4c4c1d6fce568b0d8e34b5baad5f3dd1fcce8472
AlexaKelemen <kelemenalexa85@gmail.com>
2024-10-30 10:55:53 +0100

    comment
    comment

--- BlazorApp1/Services/HttpCommentService.cs
+++ BlazorApp1/Services/HttpCommentService.cs
@@ -1,6 +1,90 @@
-﻿namespace BlazorApp1.Services;
+﻿using System.Text.Json;
+using DataTransferObjects;
 
-public class HttpCommentService
+namespace BlazorApp1.Services;
+
+public class HttpCommentService : ICommentService
 {
-    
+    private readonly HttpClient client;
+
+    private HttpCommentService(HttpClient httpClient)
+    {
+        this.client = client;
+    }
+
+    public async Task<CommentDTO> AddCommentAsync(CreateCommentDto request)
+    {
+        HttpResponseMessage httpResponse =
+            await client.PostAsJsonAsync("comments", request);
+        string response = await httpResponse.Content.ReadAsStringAsync();
+        if (!httpResponse.IsSuccessStatusCode)
+        {
+            throw new Exception(response);
+        }
+
+        return JsonSerializer.Deserialize<CommentDTO>(response,
+            new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            })!;
+    }
+
+    public async Task UpdateCommentAsync(int id,
+        ReplaceCommentDTO request)
+    {
+        HttpResponseMessage response =
+            await client.PutAsJsonAsync($"comments/{id}", request);
+        string responseString = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(responseString);
+        }
+    }
+
+    public async Task DeleteCommentAsync(int id)
+    {
+        HttpResponseMessage response =
+            await client.DeleteAsync($"comments/{id}");
+        string responseString = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(responseString);
+        }
+    }
+
+    public async Task<CommentDTO> GetCommentByIdAsync(int id)
+    {
+        HttpResponseMessage response = await client.GetAsync($"users/{id}");
+        string responseString = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(responseString);
+        }
+
+        return JsonSerializer.Deserialize<CommentDTO>(responseString,
+            new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            })!;
+    }
+
+    public async Task<List<CommentDTO>> GetAllCommentsAsync()
+    {
+        HttpResponseMessage response = await client.GetAsync("users");
+        string responseString = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(responseString);
+        }
+
+        return JsonSerializer.Deserialize<List<CommentDTO>>(responseString,
+            new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            })!;
+    }
 }
--- BlazorApp1/Services/ICommentService.cs
+++ BlazorApp1/Services/ICommentService.cs
@@ -1,6 +1,15 @@
-﻿namespace BlazorApp1.Services;
+﻿using DataTransferObjects;
+
+namespace BlazorApp1.Services;
 
 public interface ICommentService
 {
-    
+    public Task<CommentDTO> AddCommentAsync(CreateCommentDto request);
+
+    public Task UpdateCommentAsync(int id,
+        ReplaceCommentDTO request);
+
+    public Task DeleteCommentAsync(int id);
+    public Task<CommentDTO> GetCommentByIdAsync(int id);
+    public Task<List<CommentDTO>> GetAllCommentsAsync();
 }
