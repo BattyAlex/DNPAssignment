5cdd90938b756b2607af3ae8490057c5768594fc
SophiaJustin <150365571+SophiaJustin@users.noreply.github.com>
2024-10-30 10:36:10 +0100

    Services

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -5,6 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:5001")
+});
 
 var app = builder.Build();
 
