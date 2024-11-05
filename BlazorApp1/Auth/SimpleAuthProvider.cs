79bc8ce22db4245edf99853e9331ac38e5379eac
SophiaJustin <150365571+SophiaJustin@users.noreply.github.com>
2024-11-05 10:54:23 +0100

    SIMPLE AUTH

--- BlazorApp1/Auth/SimpleAuthProvider.cs
+++ BlazorApp1/Auth/SimpleAuthProvider.cs
@@ -1,48 +1,89 @@
-﻿/*using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Text.Json;
 using DataTransferObjects;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.JSInterop;
 
 namespace BlazorApp1.Auth;
 
 public class SimpleAuthProvider : AuthenticationStateProvider
 {
     private readonly HttpClient httpClient;
+    private readonly IJSRuntime jsRuntime;
     private ClaimsPrincipal currentClaimsPrincipal;
 
-    public SimpleAuthProvider(HttpClient httpClient)
+    public SimpleAuthProvider(HttpClient httpClient, IJSRuntime jsRuntime)
     {
         this.httpClient = httpClient;
+        this.jsRuntime = jsRuntime;
     }
+
     public async Task Login(string userName, string password)
     {
-        HttpResponseMessage response = await httpClient.PostAsJsonAsync( "auth/login",
-            new LoginRequest(userName, password)); 
+        HttpResponseMessage response = await httpClient.PostAsJsonAsync("auth/login",
+            new LoginRequestDTO()
+            {
+                username = userName,
+                password = password
+            });
         string content = await response.Content.ReadAsStringAsync();
 
         if (!response.IsSuccessStatusCode)
         {
-            throw new Exception(content); 
-            
-        } 
+            throw new Exception(content);
+
+        }
+
         UserDTO userDto = JsonSerializer.Deserialize<UserDTO>(content, new JsonSerializerOptions
         {
             PropertyNameCaseInsensitive = true
         })!;
-
+        string serialisedData = JsonSerializer.Serialize(userDto);
+        await jsRuntime.InvokeVoidAsync("sessionStorage.setItem", "currentUser", serialisedData);
         List<Claim> claims = new List<Claim>()
         {
             new Claim(ClaimTypes.Name, userDto.Username),
-            new Claim(“Id”, userDto.Id.ToString())
+            new Claim(ClaimTypes.NameIdentifier, userDto.Id.ToString())
         };
 
-            ClaimsIdentity identity = new ClaimsIdentity(claims, "apiauth");
+        ClaimsIdentity identity = new ClaimsIdentity(claims, "apiauth");
+        currentClaimsPrincipal = new ClaimsPrincipal(identity);
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(currentClaimsPrincipal)));
+    }
 
-            currentClaimsPrincipal = new ClaimsPrincipal(identity); NotifyAuthenticationStateChanged( Task.FromResult(new AuthenticationState(currentClaimsPrincipal)) ); }
-    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
     {
-        throw new NotImplementedException(); 
-        
-    } 
-}*/
+        string userAsJson = "";
+        try
+        {
+            userAsJson = await jsRuntime.InvokeAsync<string>("sessionStorage.getItem", "currentUser");
+        }
+        catch (InvalidOperationException e)
+        {
+            return new AuthenticationState(new());
+        }
+
+        if (string.IsNullOrEmpty(userAsJson))
+        {
+            return new AuthenticationState(new());
+
+        }
+
+        UserDTO userDto = JsonSerializer.Deserialize<UserDTO>(userAsJson)!;
+        List<Claim> claims = new List<Claim>()
+        {
+            new Claim(ClaimTypes.Name, userDto.Username), new Claim(ClaimTypes.NameIdentifier, userDto.Id.ToString()),
+        };
+        ClaimsIdentity identity = new ClaimsIdentity(claims, "apiauth");
+        currentClaimsPrincipal = new ClaimsPrincipal(identity);
+        return new AuthenticationState(currentClaimsPrincipal);
+
+    }
+
+    public async Task Logout()
+    {
+        await jsRuntime.InvokeVoidAsync("sessionStorage.setItem", "currentUser", "");
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(new())));
+    }
+}
